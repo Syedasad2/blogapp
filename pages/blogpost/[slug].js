9f8a6f2cb65557ddb75a9c944df1a5c14489733e
Syedasad2 <154581585+Syedasad2@users.noreply.github.com>
2025-04-21 13:49:06 +0500

    changes

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router';
 import styles from '../../styles/Blogpost.module.css'
 const slug  = () => {
-    const router = useRouter();
-    const {slug} = router.query;
+  const [blog, setBlog] = useState();
+  const router = useRouter();
+    useEffect(() => {
+      if (!router.isReady) return;
+      
+      const {slug} = router.query;
+       fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+         .then((a) => {
+           return a.json();
+         })
+         .then((parsed) => {
+           setBlog(parsed);
+         });
+     }, [router.isReady])
+   
   return (
     <div className={styles.container}>
       <main className=' flex flex-col gap-8 row-start-2 items-center pb-40 pt-10 justify-items-center'>
         <h1>
-          Title of the Page{slug}
+          {blog && blog.title}
         </h1>
         <hr/>
         <div className={styles.content}>
-          <p> Learn modern JavaScript (ES2015+) from scratch, and practice in an intuitive environment. The challenges are inspired by real-world projects to make sure that you're learning the best practices, one step at a time. Try the first 77 lessons, challenges, projects (first 7 chapters) & flashcards for free. You can upgrade to a Pro account with a one-time payment that gives you access for 5 years.</p>
+          <p>{blog && blog.content}</p>
        </div>
       </main>
     </div>
