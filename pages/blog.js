e87ff141650dc40189cb5c665bc64bff4dc15a34
Syedasad2 <154581585+Syedasad2@users.noreply.github.com>
2025-04-23 00:47:31 +0500

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styles from "../styles/Blog.module.css";
 import Link from "next/link";
+const Blog = (props) => {
+  const [blogs, setBlogs] = useState(props.allBlogs);
   // useEffect(() => {
   //   fetch("http://localhost:3000/api/blogs")
   //     .then((a) => {
