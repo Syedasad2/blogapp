f9c0c9da1609c4dc7741bb15dd9a1708084ab825
Syedasad2 <154581585+Syedasad2@users.noreply.github.com>
2025-05-10 01:41:18 +0500

    error fixing

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import Navbar from "./component/Navbar";
-import Footer from "./component/Footer";
+import Footer from "./component/footer";
 
 export default function App({ Component, pageProps }) {
   return <>
