2ace858c1684cb58175ac4232b689ad7944d694f
Syedasad2 <154581585+Syedasad2@users.noreply.github.com>
2025-05-12 01:51:45 +0500

    some changes in slug

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -29,17 +29,12 @@ const Slug = (props) => {
 };
 
 export async function getStaticPaths() {
+  let allb = await fs.promises.readdir(`blogdata`);
+  allb = allb.map((item)=>{
+    return  { params: { slug: item.split(".")[0]}}
+  })
   return {
-    paths: [
-      { params: { slug: "how-to-learn-flask" } },
-      { params: { slug: "how-to-learn-javascript" } },
-      { params: { slug: "how-to-learn-nextjs" } },
-      { params:  {slug: "how-to-learn-nodejs"} },
-      { params: { slug: "how-to-learn-python" } },
-      { params: { slug: "how-to-learn-cybersecurity" } },
-      { params: { slug: "how-to-learn-css" } },
-      { params:  {slug: "how-to-learn-programing"} },
-    ],
+    paths: allb,
     fallback: true,
   };
 }
